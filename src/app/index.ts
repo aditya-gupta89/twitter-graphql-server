7082120d655dc93c5c576e5bd459b6fd25fc38a0
Aditya <aditya.gupta@thinkitive.com>
2023-09-11 02:14:24 +0530

    feat : configuration with prisma client

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,10 +2,14 @@ const express = require("express");
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import bodyParser from "body-parser";
+import { prismaClient } from "../client/db";
 
 export async function initServer() {
   const app = express();
   app.use(bodyParser.json());
+  app.get("/", (req: any, res: any) => {
+    res.json("Hello Aditya Gupta");
+  });
   const graphqlServer = new ApolloServer({
     typeDefs: `
     type Query{
